623f0ac7d1e85713b128b2ca57ea7def6a91678f
Arathi-Prasanthan <prasanthanarathi91@gmail.com>
2026-08-03 21:20:17 +0530

    Add mini assignment for tuple, set, list, and dictionary manipulation

---
+++ day8.py
@@ -0,0 +1,59 @@
+# 🎯 Mini Assignment
+
+# Write a program that:
+
+# Creates a tuple named subjects containing:
+# Python
+# AI
+# Machine Learning
+# Print:
+# The complete tuple
+# The first subject
+# The last subject
+# The total number of subjects
+
+# subjects=("Python","AI","Machine Learning")
+# print("The complete tuple is :",subjects)
+# print("The first subject is :",subjects[0])
+# print("The last subject is :",subjects[-1])
+# print("The total number of subjects are:",len(subjects))
+
+# Mini Assignment
+
+# Write a program that:
+
+# Creates a set:
+# Python
+# AI
+# Machine Learning
+# Python
+# Print the set.
+# Add "Deep Learning".
+# Remove "AI".
+# Print the updated set
+
+# Courses={"Python","AI","Machine Learning","Python"}
+# print("The set is:",Courses)
+# Courses.add("Deep Learning")
+# Courses.remove("AI")
+# print("The updated set is:",Courses)
+
+# Final Mini Project
+
+# Write a program that:
+
+# Creates:
+# A list of three fruits.
+# A tuple of three colors.
+# A set of programming languages (include one duplicate).
+# A dictionary with your name and age.
+# Print all four collections.
+
+# fruits=["apple","orange","banana"]
+# colors=("red","green","blue")
+# programming_languages={"HTML","CSS","Bootstrap","Bootstrap","Java"}
+# details={"name":"arathi","age":23}
+# print(fruits)
+# print(colors)
+# print(programming_languages)
+# print(details)
